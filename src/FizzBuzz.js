eb442e06afe98a226a2e1dfd8194316f0f9efc41
Adrian Rodriguez <aaadrianrodriguezmontecinos@gmail.com>
2025-09-05 22:42:28 -0400

    feat: Convierte un 3 a Fizz

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,21 +1,5 @@
-function ConvertirFizzBuzz(n) {
-    if(n % 15 === 0){
-        return "FizzBuzz";
-    }
-    if (n % 3 === 0) {
-        return "Fizz";
-    }
-    if(n %5 === 0){
-        return "Buzz";
-    }
-  return n.toString();
-}
-function SecuenciaFizzBuzz(lim) {
-    let resultado = [];
-    for (let i = 1; i <= lim; i++) {
-        resultado.push(ConvertirFizzBuzz(i));
-    }
-    return resultado.join(", ");
-}
+function ConvertirFizzBuzz(numero){
+    return "Fizz";
+};
 
-export {ConvertirFizzBuzz, SecuenciaFizzBuzz};
+export {ConvertirFizzBuzz}
--- src/FizzBuzz.spec.js
+++ src/FizzBuzz.spec.js
@@ -1,43 +1,7 @@
-import { ConvertirFizzBuzz, SecuenciaFizzBuzz } from "./FizzBuzz.js";
-
-describe ("FizzBuzz", () => {
-  it("deberia convertir 3 a Fizz", () => {
-    expect(ConvertirFizzBuzz(3)).toBe("Fizz");
-  });
-});
-
- describe("FizzBuzz", () => {
-   it("deberia convertir un 'n' a Fizz o Buzz dependiendo las reglas", () => {
-     expect(ConvertirFizzBuzz(1)).toBe("1");
-   });
- });
-
- describe("FizzBuzz", () => {
-   it("deberia convertir cualquier multiplo de 3 a Fizz", () => {
-     expect(ConvertirFizzBuzz(9)).toBe("Fizz");
-   });
-});
-
- describe("FizzBuzz", () => {
-   it("deberia convertir 5 a Buzz", () => {
-     expect(ConvertirFizzBuzz(5)).toBe("Buzz");
-   });
-});
-
- describe("FizzBuzz", () => {
-   it("deberia convertir cualquier multiplo de 5 a Buzz", () => {
-     expect(ConvertirFizzBuzz(10)).toBe("Buzz");
-   });
-});
-
- describe("FizzBuzz", () => {
-   it("deberia convertir cualquier multiplo de 3 y 5 a FizzBuzz", () => {
-     expect(ConvertirFizzBuzz(15)).toBe("FizzBuzz");
-   });
-});
+import { ConvertirFizzBuzz } from "./FizzBuzz.js";
 
 describe("FizzBuzz", () => {
-  it("deberia mostrar una secuencia hasta 10", () => {
-    expect(SecuenciaFizzBuzz(10)).toBe("1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz");
-  });
+  it("Deberia devolver 3 Fizz", () => {
+    expect(ConvertirFizzBuzz(3)).toEqual("Fizz");
+  })
 });
