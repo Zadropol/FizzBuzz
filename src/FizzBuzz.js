23eefb4e51e33e1083be90e764e68a713d5b34d5
Adrian Rodriguez <aaadrianrodriguezmontecinos@gmail.com>
2025-09-05 22:49:50 -0400

    fix: ahora convierte un 3 a Fizz y un n a literal

--- src/FizzBuzz.js
+++ src/FizzBuzz.js
@@ -1,4 +1,8 @@
 function ConvertirFizzBuzz(numero){
+    if(numero %3 ===0)
+    {
+        return "Fizz";
+    }
     return numero.toString();
 };
 
--- src/FizzBuzz.spec.js
+++ src/FizzBuzz.spec.js
@@ -1,14 +1,14 @@
 import { ConvertirFizzBuzz } from "./FizzBuzz.js";
 
 describe("FizzBuzz", () => {
-  it("Deberia devolver 3 Fizz", () => {
-    expect(ConvertirFizzBuzz(3)).toBe("Fizz");
+  it("Deberia devolver 1 a 1 en forma literal", () => {
+    expect(ConvertirFizzBuzz(1)).toBe("1");
   });
 });
 
 describe("FizzBuzz", () => {
-  it("Deberia devolver 1 a 1 en forma literal", () => {
-    expect(ConvertirFizzBuzz(1)).toBe("1");
+  it("Deberia devolver 3 Fizz", () => {
+    expect(ConvertirFizzBuzz(3)).toBe("Fizz");
   });
 });
 
